9b22e289355710e77c201f38635cf6f71cfa8808
JesseJohn7 <Jessejohn260@gmail.com>
2026-05-06 10:51:48 +0100

    people

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -47,7 +47,7 @@ export default function Testimonial() {
       <div className="max-w-6xl mx-auto">
 
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
-          What Developers Are Saying
+          What People Are Saying
         </h2>
 
         <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
